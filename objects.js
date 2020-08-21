48f87b3d983217bdf3ceb0ed4ffea506ea996dba
Steven Quach <stevendquach@gmail.com>
2020-08-21 12:48:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = {
 }
 
 
-function updatePlaylist (playlist, 'BSB' , 'Life') {
-  playlist['BSB']= 'Life'
+function updatePlaylist (playlist, BSB , 'Life') {
+  playlist[BSB]= 'Life'
   return playlist
 }
