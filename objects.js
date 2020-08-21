b0246c0a31596842e3d307a9609819e0dd7ae5fc
Steven Quach <stevendquach@gmail.com>
2020-08-21 12:16:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ var playlist = {
   MCR : "I'm Ok"
 }
 
-funnction() updatePlaylist (playlist, BSB, BSB is Back) {
+funnction updatePlaylist(playlist, BSB, BSB is Back) {
   playlist ['BSB'] = 'BSB is Back' };
